eb3e7d6e656fea3478ce929ca89286dfa45ad062
Alexandr Bolbat <alexandr@bolbat.net>
2014-10-16 17:37:31 +0300

    added junits with TODO's for Locking functionality

---
+++ test/junit/net/bolbat/kit/vo/LockingEntityVOTest.java
@@ -0,0 +1,20 @@
+package net.bolbat.kit.vo;
+
+import org.junit.Test;
+
+/**
+ * {@link LockingEntityVO} test.
+ * 
+ * @author Alexandr Bolbat
+ */
+public class LockingEntityVOTest {
+
+	/**
+	 * Complex test.
+	 */
+	// TODO Implement me
+	@Test
+	public void complexTest() {
+	}
+
+}
---
+++ test/junit/net/bolbat/kit/vo/LockingVOTest.java
@@ -0,0 +1,20 @@
+package net.bolbat.kit.vo;
+
+import org.junit.Test;
+
+/**
+ * {@link LockingVO} test.
+ * 
+ * @author Alexandr Bolbat
+ */
+public class LockingVOTest {
+
+	/**
+	 * Complex test.
+	 */
+	// TODO Implement me
+	@Test
+	public void complexTest() {
+	}
+
+}
